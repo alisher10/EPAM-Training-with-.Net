1af81d9dcf1c3960c863d67191ad484c14a49363
Alisher Khamidov <85631085+alisher10@users.noreply.github.com>
2022-12-28 18:32:17 +0500

    OOPTask solution

---
+++ OOPTask/OOPTask/Chassis.cs
@@ -0,0 +1,26 @@
+﻿namespace OOPTask
+{
+    public class Chassis
+    {
+        public int WheelsNumber { get; }
+
+        public int PermissibleLoad { get; }
+
+        public string ChassisNumber { get; }
+
+        public Chassis(int wheelsNumber, int permissibleLoad, string chassisNumber) {
+
+            if (WheelsNumber > 0 && PermissibleLoad > 0 && ChassisNumber.Length > 0)
+            {
+                WheelsNumber = wheelsNumber;
+                PermissibleLoad = permissibleLoad;
+                ChassisNumber = chassisNumber;
+            }
+            else {
+                WheelsNumber = 0;
+                PermissibleLoad = 0;
+                ChassisNumber = "SNDEFAULT";
+            }
+        }
+    }
+}
